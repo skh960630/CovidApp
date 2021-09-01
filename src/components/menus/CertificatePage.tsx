a7c560c3aaf78edc10bc4cf68f2006efa9a40cb4
Kane <skh960630@gmail.com>
2021-09-01 21:31:07 +1000

    Update certificate page

--- src/components/menus/CertificatePage.tsx
+++ src/components/menus/CertificatePage.tsx
@@ -1,17 +1,25 @@
 import React from 'react';
-import { Center, Container, Box, Stack } from "native-base";
+import { Center, Box, Text, Button } from "native-base";
 
 export default function CertificatePage ({route, navigation}: {route: any, navigation: any}) {
     return(
-        <Container bg={{
-            linearGradient: {
-            colors: ["lightBlue.300", "violet.800"],
-            start: [0, 0],
-            end: [1, 0],
-            },
-        }}>
-            <Center style={{ marginTop: '10%' }}>
-            </Center>
-        </Container>
+        <Center>
+            <Box
+                bg={{
+                    linearGradient: {
+                    colors: ["lightBlue.300", "violet.800"],
+                    start: [0, 0],
+                    end: [1, 0],
+                    },
+                }}
+                p={12}
+                style={{ height: '100%', width: '100%' }}
+                >
+                <Box bg='white' width={500} shadow={1} rounded="lg" style={{ height: '90%', width: '100%' }}>
+                    <Text>HELLO</Text>
+                    <Button>Back to the list</Button>
+                </Box>
+            </Box>
+        </Center>
     );
 }
